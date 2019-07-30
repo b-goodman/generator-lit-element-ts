3bf25e5566b79da4069c4f9d52708fb965013e46
Ben Goodman <bgoodman@wolfram.com>
2019-07-29 23:27:27 -0400

    adding img asset option

--- app/templates/rollup.config.js
+++ app/templates/rollup.config.js
@@ -1,6 +1,7 @@
 import createDefaultConfig from '@open-wc/building-rollup/modern-and-legacy-config.js';
 import litSass from '@ponday/rollup-plugin-lit-sass';
 import cjs from "rollup-plugin-cjs-es";
+import cpy from "rollup-plugin-cpy";
 
 const configs = createDefaultConfig({
     input: './index.html',
@@ -11,6 +12,11 @@ export default configs.map(config => ({
     ...config,
     plugins: [
         ...config.plugins,
+        cpy({
+            // copy over any/all images files
+            files: ["./src/img/*.png"],
+            dest: "./dist/img",
+        }),
         cjs({
 			nested: true,
 			exclude: [ "**/*.css", "**/*.scss", "**/*.html",  ],
